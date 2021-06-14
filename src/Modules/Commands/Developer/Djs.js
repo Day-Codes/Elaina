e33eb0e1ef5fdff8e3e02d1a7cd1d6521e6e02b4
Rygent <marizky65@gmail.com>
2021-06-14 16:56:34 +0700

    Djs: Change result to master branch
    Signed-off-by: Rygent <marizky65@gmail.com>

--- src/Modules/Commands/Developer/Djs.js
+++ src/Modules/Commands/Developer/Djs.js
@@ -20,13 +20,15 @@ module.exports = class extends Command {
 		if (!query) return message.reply('Please specify a valid query to search!');
 
 		const headers = { 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36' };
-		const result = await axios.get(`https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(query)}`, { headers }).then(res => res.data);
+		const result = await axios.get(`https://djsdocs.sorta.moe/v2/embed?src=master&q=${encodeURIComponent(query)}`, { headers }).then(res => res.data);
 
 		if (!result || result.error) {
 			return message.reply(`\`${query}\` couldn't be located within the discord.js documentation (<https://discord.js.org/>).`);
 		}
 
 		const embed = new MessageEmbed(result)
+			.setAuthor(result.author.name, 'https://discord.js.org/favicon-32x32.png', result.author.url)
+			.setColor('#5865F2')
 			.setFooter(`Responded in ${this.client.utils.responseTime(message)} | Powered by Discord.js`, message.author.avatarURL({ dynamic: true }));
 
 		if (!message.guild) {
